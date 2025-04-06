911dcff5342638e87b70599b63f95cfda275659b
revoly afanasev <rewoly@mail.ru>
2025-04-06 13:16:31 +0900

    Работа с логами

--- Server/src/main/java/com/example/Server/WebController.java
+++ Server/src/main/java/com/example/Server/WebController.java
@@ -1,12 +1,20 @@
 package com.example.Server;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class WebController {
-    @GetMapping("/")
-    private String getInfo() {
-        return "Аналитическая панель для мониторинга поведения пользователей в приложении";
+    @Autowired
+    private LogActionService logActionService;
+    private List<LogActionEntity> logActionEntityList = new ArrayList<>();
+    @GetMapping("/getLogs")
+    private List<LogActionEntity> getInfo() {
+        logActionEntityList = logActionService.readLogActions();
+        return logActionEntityList;
     }
 }
